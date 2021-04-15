7aa542f342aae67529e5c430d161bf86e76dd6ec
Tahereh <75454017+tjavaheripour@users.noreply.github.com>
2021-04-14 23:38:56 -0500

    upload Deliverable2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -31,3 +31,22 @@ FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT (ut.emp_no) DESC;
 SELECT * from retiring_titles
+
+-- DELIVERABLE 2 
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE de.to_date = ('9999-01-01')
+    AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+SELECT * from mentorship_eligibility
